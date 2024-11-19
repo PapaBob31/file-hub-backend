09c6844570536aef430c60bb9932eb1b4123f745
Adedamola Egbebi <egbebitimi121@gmail.com>
2024-11-19 12:45:25 +0100

    Changed endpoint name

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ const router = Router()
 
 router.post("/login", loginHandler);
 router.post("/signup", signupHandler)
-router.post("/upload-files", fileUploadHandler); // renmae this route since only one file is being uploaded at a time;
+router.post("/upload-file", fileUploadHandler); // rename this route since only one file is being uploaded at a time;
 router.get("/fileDetail/:fileHash", fileReqByHashHandler)
 router.get("/files-data", filesRequestHandler)
 router.get("/images/:fileUrl", imgReqHandler)
