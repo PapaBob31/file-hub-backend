62c71a81408fac13189798ccf3d5c107e6c7516d
Adedamola Egbebi <egbebitimi121@gmail.com>
2024-11-21 23:24:37 +0100

    Added a new 'create-folders' route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import {loginHandler, signupHandler, fileUploadHandler, fileReqByHashHandler, filesRequestHandler, imgReqHandler } from "../controllers";
+import {
+	loginHandler, signupHandler, fileUploadHandler, fileReqByHashHandler, 
+	filesRequestHandler, fileReqHandler, authHandler, createFolderReqHandler
+} from "../controllers";
+
 const router = Router()
 
 router.post("/login", loginHandler);
 router.post("/signup", signupHandler)
-router.post("/upload-file", fileUploadHandler); // rename this route since only one file is being uploaded at a time;
+router.post("/auth-user", authHandler)
+router.post("/:folderUri/upload-file", fileUploadHandler); // rename this route since only one file is being uploaded at a time;
 router.get("/fileDetail/:fileHash", fileReqByHashHandler)
-router.get("/files-data", filesRequestHandler)
-router.get("/images/:fileUrl", imgReqHandler)
+router.get("/:folderUri/files-data", filesRequestHandler)
+router.get("/files/:fileUri", fileReqHandler)
+router.post("/create-folder", createFolderReqHandler)
 
 export default router;
