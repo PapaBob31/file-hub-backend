8d268a961c9485a6a390443fef417909c3dbd397
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-03-04 16:31:20 +0100

    Add middleware that restricts the content express.json middleware can parse
    Added middleware that makes sure expresss.json can only attempt to
    parse http requests without a file payload. This is needed because
    express.json usually parses files with content-type of 'application/json'
    and that is not what I want

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
 
 // TODO: uninstall all unused packages
-import express from "express"
+import express, {Response, Request}  from "express"
 import cookieParser from "cookie-parser";
 import {setCorsHeaders, logRequestDetails, authenticateUser, loginSession, checkForCSRF } from "./middlewares/index.js";
 import router from "./routes/index.js"
@@ -9,14 +9,19 @@ import {htmlFileReqHandler} from "./controllers/index.js"
 const app = express()
 const portNo = 7200;
 
+// prevents the express.json middleware from parsing an uploaded json type with content-type of 'application/json'
+function parseOnlyIfJsonRequest(req: Request, res: Response, next: ()=>void) {
+	if (!req.headers['x-local-name']) { // request is a file upload
+		express.json({limit: "1000kb"})(req, res, next);
+	}else next()
+}
 
 app.use(loginSession)
 app.use(cookieParser())
 app.use(logRequestDetails)
 app.use(express.static("../static"))
 app.use("/services", setCorsHeaders, authenticateUser, checkForCSRF)
-app.use(express.json())
-app.use('/services', router);
+app.use("/services", parseOnlyIfJsonRequest, router)
 app.all("/*", htmlFileReqHandler)
 
 
